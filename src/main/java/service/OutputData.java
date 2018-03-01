ab2dbf8b3c30221c6d28130d28d0fe1f384f9054
Mitja Horvat <mitja.horvat.92@gmail.com>
2018-03-01 22:43:13 +0100

    Output not finished

--- src/main/java/service/OutputData.java
+++ src/main/java/service/OutputData.java
@@ -2,9 +2,43 @@ package service;
 
 import models.Vehicle;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class OutputData {
+
+
+//    int numberofvehicles = Integer.parse
+//
+//
+//    Vehicle vehicle = new Vehicle();
+//    int vehicle1 = vehicle.getVehicleId();
+//    String vehicleIdString = String.valueOf(vehicle1);
+//
+//    int vehicle2 = vehicle.getNumOfRides();
+//    String numOfRidesString = String.valueOf(vehicle2);
+    public static List<String> output(String blabla){
+        
+        List<String> output = new List<String>;
+
+        List<Vehicle> vehicles = new ArrayList<>();
+        for(Vehicle vehicle: List<Vehicle>){
+            Vehicle vehicle = new Vehicle();
+            vehicle.getNumOfRides();
+            vehicle.getRideIds();
+            vehicles.add(vehicle);
+        }
+        return output;
+    }
+
+
+
+
+
+
+
 
 
 }
