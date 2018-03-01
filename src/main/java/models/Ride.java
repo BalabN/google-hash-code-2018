0cb05fda41d8124505a008adb7169c37acaa357f
marko.balazic <mbalaic3@gmail.com>
2018-03-01 20:39:42 +0100

    added ride model

--- src/main/java/models/Ride.java
+++ src/main/java/models/Ride.java
@@ -1,14 +1,52 @@
 package models;
 
-public class Ride {
-    private int earlyStart;
-    private int end
+import javafx.util.Pair;
 
-    public int getEarlyStart() {
-        return earlyStart;
+
+public class Ride {
+    private int earlyStartTime;
+    private int deadLineTime;
+    private Pair<Integer, Integer> startLocation;
+    private Pair<Integer, Integer> endLocation;
+
+    public Ride(){}
+
+    public Ride (int earlyStartTime, int deadLineTime, Pair<Integer, Integer> startLocation, Pair<Integer, Integer> endLocation){
+        this.earlyStartTime = earlyStartTime;
+        this.deadLineTime = deadLineTime;
+        this.startLocation = startLocation;
+        this.endLocation = endLocation;
     }
 
-    public void setEarlyStart(int earlyStart) {
-        this.earlyStart = earlyStart;
+    public int getEarlyStartTime() {
+        return earlyStartTime;
+    }
+
+    public void setEarlyStartTime(int earlyStartTime) {
+        this.earlyStartTime = earlyStartTime;
+    }
+
+    public int getDeadLineTime() {
+        return deadLineTime;
+    }
+
+    public void setDeadLineTime(int deadLineTime) {
+        this.deadLineTime = deadLineTime;
+    }
+
+    public Pair<Integer, Integer> getStartLocation() {
+        return startLocation;
+    }
+
+    public void setStartLocation(Pair<Integer, Integer> startLocation) {
+        this.startLocation = startLocation;
+    }
+
+    public Pair<Integer, Integer> getEndLocation() {
+        return endLocation;
+    }
+
+    public void setEndLocation(Pair<Integer, Integer> endLocation) {
+        this.endLocation = endLocation;
     }
 }
