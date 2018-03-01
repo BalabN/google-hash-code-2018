69ab3c3f29b19e81357d7380e7179b91bb62eb1e
Nikola Balaban <nikola.balaban@outfit7.com>
2018-03-01 18:08:35 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import java.util.List;
+
+import logic.LinearRegression;
+import logic.NaiveBayes;
+import models.Transaction;
+import retrofit2.Response;
+import service.BitstampService;
+import service.ServiceGenerator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BitstampService bitstampService = ServiceGenerator.createService(BitstampService.class, ServiceGenerator.ApiEndpoint.BITSTAMP);
+
+        Response<List<Transaction>> response = null;
+        try {
+            response = bitstampService.getTransactions().execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (response != null && response.isSuccessful()) {
+            List<Transaction> transaction = response.body();
+            System.out.println(response.body());
+            LinearRegression linearRegression = new LinearRegression(transaction);
+            NaiveBayes naiveBayes = new NaiveBayes();
+        } else if (response != null) {
+            System.out.println("Response failed with error code = " + response.code() + "; message = " + response.errorBody());
+        } else {
+            System.out.println("Response = null");
+        }
+    }
+}
