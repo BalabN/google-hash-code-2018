53db42cae2d07d5051bc607bf20c4dc73990ad81
Nikola Balaban <nikola.balaban1@gmail.com>
2018-03-01 19:44:47 +0100

    CleanUp main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,9 @@
-import java.util.List;
-
-import logic.LinearRegression;
-import logic.NaiveBayes;
-import models.Transaction;
 import models.World;
-import retrofit2.Response;
-import service.BitstampService;
 import service.ParseInput;
-import service.ServiceGenerator;
 
 public class Main {
-
-    /*public static void main(String[] args) {
-        BitstampService bitstampService = ServiceGenerator.createService(BitstampService.class, ServiceGenerator.ApiEndpoint.BITSTAMP);
-
-        Response<List<Transaction>> response = null;
-        try {
-            response = bitstampService.getTransactions().execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (response != null && response.isSuccessful()) {
-            List<Transaction> transaction = response.body();
-            System.out.println(response.body());
-            LinearRegression linearRegression = new LinearRegression(transaction);
-            NaiveBayes naiveBayes = new NaiveBayes();
-        } else if (response != null) {
-            System.out.println("Response failed with error code = " + response.code() + "; message = " + response.errorBody());
-        } else {
-            System.out.println("Response = null");
-        }
-    }*/
-
     public static void main(String[] args) throws Exception {
-        if(args.length < 1) {
+        if (args.length < 1) {
             throw new Exception("Invalid number of input args");
         }
         String fileName = args[0];
