2c1efff1721614b815acd07e3a13eed9004f1545
pr3mar <markoprelevik@gmail.com>
2018-03-01 19:43:40 +0100

    Parse input structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,15 @@ import java.util.List;
 import logic.LinearRegression;
 import logic.NaiveBayes;
 import models.Transaction;
+import models.World;
 import retrofit2.Response;
 import service.BitstampService;
+import service.ParseInput;
 import service.ServiceGenerator;
 
 public class Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         BitstampService bitstampService = ServiceGenerator.createService(BitstampService.class, ServiceGenerator.ApiEndpoint.BITSTAMP);
 
         Response<List<Transaction>> response = null;
@@ -29,5 +31,13 @@ public class Main {
         } else {
             System.out.println("Response = null");
         }
+    }*/
+
+    public static void main(String[] args) throws Exception {
+        if(args.length < 1) {
+            throw new Exception("Invalid number of input args");
+        }
+        String fileName = args[0];
+        World world = ParseInput.parseInput(fileName);
     }
 }
--- src/main/java/service/ParseInput.java
+++ src/main/java/service/ParseInput.java
@@ -1,7 +1,33 @@
 package service;
 
-public class ParseInput {
-    public static void parseInput(String inputFileName) {
+import models.Car;
+import models.Ride;
+import models.World;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ParseInput {
+
+    public static World parseInput(String inputFileName) throws IOException{
+        System.out.println("Reading file " + inputFileName);
+        List<String> content = new ArrayList<String>();
+        Stream<String> stream = Files.lines(Paths.get(inputFileName));
+        content = stream.collect(Collectors.toList());
+        String[] firstLine = content.get(0).split(" ");
+        int rows = Integer.parseInt(firstLine[0]);
+        int cols = Integer.parseInt(firstLine[1]);
+        int cars = Integer.parseInt(firstLine[2]);
+        int rides = Integer.parseInt(firstLine[3]);
+        int bonus = Integer.parseInt(firstLine[4]);
+        int steps = Integer.parseInt(firstLine[5]);
+
+        return new World(rows, cols, new ArrayList<Car>(), new ArrayList<Ride>(), bonus, steps);
     }
 }
