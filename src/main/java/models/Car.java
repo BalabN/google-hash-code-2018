f0e7b8b381a2397af7f5d8382286cfd0a8be93ae
Martin Knavs <martin.knavs@gmail.com>
2018-03-01 20:56:19 +0100

    Car model

--- src/main/java/models/Car.java
+++ src/main/java/models/Car.java
@@ -1,4 +1,20 @@
 package models;
 
+import utlis.Pair;
+
 public class Car {
+    Pair<Integer, Integer> currentPosition;
+    int distanceToStart;
+    int distanceToEnd;
+    boolean isDriving;
+    int hasPassenger;
+    float k;
+
+    public Car(Pair<Integer, Integer> currentPosition, int distanceToStart, int distanceToEnd, float k) {
+        this.currentPosition = currentPosition;
+        this.distanceToStart = distanceToStart;
+        this.distanceToEnd = distanceToEnd;
+        this.k = k;
+
+    }
 }
