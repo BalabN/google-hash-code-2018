9642f6db8646d804ede10adc850d2bd9c7ec2782
Martin Knavs <martin.knavs@outfit7.com>
2018-03-01 19:55:18 +0100

    Ride model

--- src/main/java/models/Ride.java
+++ src/main/java/models/Ride.java
@@ -1,4 +1,19 @@
 package models;
 
+import utlis.Pair;
+
 public class Ride {
+    Pair<Integer, Integer> rideStart;
+    Pair<Integer, Integer> rideEnd;
+    int earliestStart;
+    int latestFinish;
+
+    public Ride(Pair<Integer, Integer> rideStart, Pair<Integer, Integer> rideEnd, int earliestStart, int latestFinish) {
+        this.rideStart = rideStart;
+        this.rideEnd = rideEnd;
+        this.earliestStart = earliestStart;
+        this.latestFinish = latestFinish;
+    }
+
+
 }
